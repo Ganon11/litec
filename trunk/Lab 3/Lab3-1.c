f17f3eb02e4b041a3f349c9f4e3e28f3c8724b48
stark3 <stark3@d152fdf6-df3c-fff5-1bc3-b889ef70f805>
2010-10-19 19:48:39 +0000

    Final version of Lab 3-1 code.

--- trunk/Lab 3/Lab3-1.c
+++ trunk/Lab 3/Lab3-1.c
@@ -9,7 +9,6 @@
 #include <stdio.h>
 #include <c8051_SDCC.h>
 
-#define PCA_COUNTS 36864
 #define PW_MIN 2027
 #define PW_NEUT 2764
 #define PW_MAX 3502
@@ -22,8 +21,13 @@ void PCA_Init (void);
 void XBR0_Init(void);
 void Interrupt_Init(void);
 void Drive_Motor(void);
+
+//-----------------------------------------------------------------------------
+// Global Variables and Constants
+//-----------------------------------------------------------------------------
 unsigned int MOTOR_PW = 0;
 unsigned int Counts = 0;
+unsigned int PCA_COUNTS = 36864;
 
 //-----------------------------------------------------------------------------
 // Main Function
@@ -33,6 +37,7 @@ void main(void) {
   Sys_Init();
   putchar(' '); // the quotes in this line may not format correctly
   Port_Init();
+  Interrupt_Init();
   XBR0_Init();
   PCA_Init();
 
@@ -41,13 +46,13 @@ void main(void) {
 
   // set initial value
   MOTOR_PW = PW_NEUT;
-  printf("MOTOR_PW = %d\r\n", MOTOR_PW);
+  PCA0CPL2 = 0xFFFF - MOTOR_PW;
+  PCA0CPH2 = (0xFFFF - MOTOR_PW) >> 8;
 
   // add code to set the servo motor in neutral for one second
   Counts = 0; // Counts is incremented once every 20 ms.  1 s = 1000 ms.  1000 / 20 = 50.  Wait 50 counts
-  while (Counts < 50) {
-    printf("Waiting...Counts = %d\r\n", Counts);
-  }
+  printf("Waiting...\r\n");
+  while (Counts < 50);
 
   printf("Done waiting 1 second.\r\n");
 
@@ -68,17 +73,17 @@ void Drive_Motor() {
   // wait for a key to be pressed
   input = getchar();
   if (input == 'f') { // if 'f' is pressed by the user
-    printf("You pressed 'f'.  Going faster.\r\n");
+    printf("\rYou pressed 'f'.  Going faster.\r\n");
     if (MOTOR_PW < PW_MAX) {
       MOTOR_PW = MOTOR_PW + 10; // increase the steering pulsewidth by 10
     }
   } else if (input == 's') { // if 's' is pressed by the user
-    printf("You pressed 's'.  Going slower.\r\n");
+    printf("\rYou pressed 's'.  Going slower.\r\n");
     if (MOTOR_PW > PW_MIN) {
       MOTOR_PW = MOTOR_PW - 10; // decrease the steering pulsewidth by 10
     }
   } else {
-    printf("You pressed '%c'.  Huh?\r\n", input);
+    printf("\rYou pressed '%c'.  Huh?\r\n", input);
   }
   printf("MOTOR_PW = %d\r\n", MOTOR_PW);
   PCA0CPL2 = 0xFFFF - MOTOR_PW;
@@ -92,7 +97,7 @@ void Drive_Motor() {
 // Set up ports for input and output
 //
 void Port_Init() {
-  P0MDOUT |= 0x40; // set output pin for CEX2 in push-pull mode
+  P1MDOUT |= 0x04; // set output pin for CEX2 in push-pull mode
 }
 
 //-----------------------------------------------------------------------------
@@ -114,7 +119,8 @@ void Interrupt_Init() {
 // Set up the crossbar
 //
 void XBR0_Init() {
-  XBR0 = 0x27; // configure crossbar with UART0, SPI, SMBus, and CEX channels as in worksheet
+               // 0001 1111
+  XBR0 = 0x27; // configure crossbar with UART0, SPI, SMBus, and CEX channels as in worksheet 7
 }
 
 //-----------------------------------------------------------------------------
@@ -127,9 +133,9 @@ void PCA_Init(void) {
   // reference to the sample code in Example 4.5 - Pulse Width Modulation implemented using the PCA (Programmable Counter Array, p. 50 in Lab Manual.
   // Use a 16 bit counter with SYSCLK/12.
   // WE WILL USE CCM 2!  Felix and Alan will use CCM 0!
-  PCA0MD = 0x81;
+  PCA0CN = 0x40;
   PCA0CPM2 = 0xC2;
-  PCA0CN |= 0x40;
+  PCA0MD = 0x81;
 }
 
 //-----------------------------------------------------------------------------
@@ -140,12 +146,12 @@ void PCA_Init(void) {
 //
 void PCA_ISR ( void ) interrupt 9 {
   if (CF) {
-    printf("INTERRUPT LOLOLOL\r\n");
     // Reset PCA to the correct start value (65,535 - PCA_COUNTS)
     PCA0L = 0xFFFF - PCA_COUNTS;
-    PCA0H = (0xFFFF - PCA_COUNTS) << 8;
+    PCA0H = (0xFFFF - PCA_COUNTS) >> 8;
     // Increment Counts variable (used for waiting for some amount of time)
     Counts++;
+    CF = 0;
   } else {
     PCA0CN &= 0xC0; // all other type 9 interrupts
   }
