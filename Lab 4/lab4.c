6b185fcc0bca6235bc2bd045ae3dbaba4581a015
stark3 <stark3@d152fdf6-df3c-fff5-1bc3-b889ef70f805>
2010-11-05 19:15:17 +0000

    Merged lab4 code.

---
+++ Lab 4/lab4.c
@@ -0,0 +1,309 @@
+/*
+ * Names: Michael Stark, David Melecio-Vazquez, Mike Wilkins, and Alan Schimmel
+ * Section: 4 (Side A)
+ * Date: 2 November 2010
+ * File name: lab4.c
+ * Program description: Combining the drive control with the steering control,
+ *     adding the LCD display, and reading the battery voltage.
+ */
+ 
+#include <stdio.h>
+#include <c8051_SDCC.h>
+#include <i2c.h>
+
+#define DRIVE_PW_MIN 2027 // 1.1 ms pulsewidth
+#define DRIVE_PW_NEUT 2764 // 1.5 ms pulsewidth
+#define DRIVE_PW_MAX 3502 // 1.9 ms pulsewidth
+
+#define STEER_PW_MIN 1659
+#define STEER_PW_NEUT 2736
+#define STEER_PW_MAX 3871
+
+//-----------------------------------------------------------------------------
+// 8051 Initialization Functions
+//-----------------------------------------------------------------------------
+void Port_Init(void); // Initialize input/output ports.
+void Interrupt_Init(void); // Set up PCA0 interrupts.
+void XBR0_Init(void); // Initialize crossbar.
+void SMB_Init(void); // Initialize system bus.
+void PCA_Init (void); // Initialize the PCA counter.
+
+//-----------------------------------------------------------------------------
+// Motor functions
+//-----------------------------------------------------------------------------
+int Read_Ranger(void); // Read the ultrasonic ranger.
+void Drive_Motor(int range); // Vary the motor PW based on the range in cm.
+
+//-----------------------------------------------------------------------------
+// Steering functions
+//-----------------------------------------------------------------------------
+unsigned int ReadCompass(void); // Read the electronic compass
+void Steer(unsigned int current_heading); // Vary the steering PW based on the heading in degrees.
+
+//-----------------------------------------------------------------------------
+// Global Variables
+//-----------------------------------------------------------------------------
+unsigned int MOTOR_PW = 0; // Pulsewidth to use for the drive motor.
+unsigned int STEER_PW = 0; // Pulsewidth to use for the steering motor
+unsigned int D_Counts = 0; // Number of overflows, used for setting new_range
+unsigned int S_Counts = 0; // 
+unsigned int Overflows = 0; // Number of overflows, used for waiting 1 second
+unsigned char new_range = 0; // flag to start new range reading
+unsigned char new_heading = 0; // flag to start new direction reading
+unsigned int PCA_COUNTS = 36864; // number of counts in 20 ms.  Constant.
+unsigned int desired_heading = 2700; // Desired direction
+sbit at 0xB6 DSS; // Slide switch controlling the Drive_Motor function
+sbit at 0xB7 SSS; // Slide switch controlling the Steer function.
+
+//-----------------------------------------------------------------------------
+// Main Function
+//-----------------------------------------------------------------------------
+void main(void) {
+  unsigned char i = 0, j = 0; // Index variable for printing range every 400 ms or so.
+  unsigned char info[1] = {'\0'}; // Data to write to the ranger
+  unsigned char addr = 0xE0; // Address of the ranger
+  int range; // Result of the read operation
+  unsigned int current_heading; // Heading read by the electronic compass
+
+  // System initialization
+  Sys_Init();
+  putchar(' ');
+  Port_Init();
+  Interrupt_Init();
+  XBR0_Init();
+  SMB_Init();
+  PCA_Init();
+
+  // print beginning message
+  printf("\rEmbedded Control Motor Controlled by Ultrasonic Ranger\r\n");
+  
+  // Signal to start a ping and record result in cm
+  info[0] = 0x51;
+
+  // set initial value
+  MOTOR_PW = DRIVE_PW_NEUT;
+  PCA0CPL2 = 0xFFFF - MOTOR_PW;
+  PCA0CPH2 = (0xFFFF - MOTOR_PW) >> 8;
+
+  Overflows = 0; // Overflows is incremented once every 20 ms.  1 s = 1000 ms.  1000 / 20 = 50.  Wait 50 counts
+  printf("Waiting 1 second...\r\n");
+  while (Overflows < 50);
+  printf("Done waiting 1 second.\r\n");
+
+  while (1) {
+    if (new_heading) {
+      j++; // Used for printing the heading every 10 times we read, or about every 480 ms.
+      current_heading = ReadCompass(); // Read the electronic compass
+      
+      if (j > 10) {
+        printf("heading read: %d degrees\r\n", current_heading);
+        j = 0;
+      }
+      
+      if (!SSS) {
+        Steer(current_heading); // Change heading based on the current heading.
+      } else {
+        // Make the wheels straight
+        PCA0CPL0 = 0xFFFF - STEER_PW_NEUT;
+        PCA0CPH0 = (0xFFFF - STEER_PW_NEUT) >> 8;
+      }
+      
+      new_heading = 0;
+    }
+    
+    if (new_range) {
+      i++; // This is used for printing the range every 6 times we read a new range, or about every 480 ms
+      range = Read_Ranger(); // Read the ultrasonic ranger
+      
+      if (i > 5) {
+        printf("range read: %d cm\r\n", range);
+        i = 0;
+      }
+      
+      if (!DSS) {
+        Drive_Motor(range); // Change the speed based on the range read.
+      } else {
+        Drive_Motor(45); // Set the motor to neutral.
+      }
+      
+      i2c_write_data(addr, 0, info, 1); // Write the ping signal to register 0 of the ranger
+      new_range = 0; // Reset the flag and wait for 80ms
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------
+// Port_Init
+//-----------------------------------------------------------------------------
+//
+// Set up ports for input and output
+//
+void Port_Init() {
+  P1MDOUT |= 0x05; // set output pin for CEX2 and CEX0 in push-pull mode
+  P3MDOUT &= 0x3F; // set input pins P3.6 and P3.7 (Slide switches) in open-drain mode
+  P3 |= 0xC0; // Set input pins P3.6 and P3.7 (Slide switches) to high impedance
+}
+
+//-----------------------------------------------------------------------------
+// Interrupt_Init
+//-----------------------------------------------------------------------------
+//
+// Enable proper interrupts.
+//
+void Interrupt_Init() {
+  // IE and EIE1
+  EA = 1; // Enable interrupts globally
+  EIE1 |= 0x08; // Enable PCA0 interrupts
+}
+
+//-----------------------------------------------------------------------------
+// XBR0_Init
+//-----------------------------------------------------------------------------
+//
+// Set up the crossbar
+//
+void XBR0_Init() {
+               // 0001 1111
+  XBR0 = 0x27; // configure crossbar with UART0, SPI, SMBus, and CEX channels as in worksheet 7
+}
+
+//-----------------------------------------------------------------------------
+// SMB_Init
+//-----------------------------------------------------------------------------
+//
+// Set up the system bus
+//
+void SMB_Init(void) {
+  SMB0CR = 0x93; // set SCL to use 100 kHz
+  ENSMB = 1; // Bit 6 of SMB0CN.  Enables the system bus.
+}
+
+//-----------------------------------------------------------------------------
+// PCA_Init
+//-----------------------------------------------------------------------------
+//
+// Set up Programmable Counter Array
+//
+void PCA_Init(void) {
+  // reference to the sample code in Example 4.5 - Pulse Width Modulation implemented using the PCA (Programmable Counter Array, p. 50 in Lab Manual.
+  // Use a 16 bit counter with SYSCLK/12.
+  // WE WILL USE CCM 2!  Felix and Alan will use CCM 0!
+  PCA0CN = 0x40;
+  PCA0CPM2 = 0xC2;
+  PCA0MD = 0x81;
+}
+
+//-----------------------------------------------------------------------------
+// Read_Ranger
+//-----------------------------------------------------------------------------
+//
+// Reads the latest ping value from the Ultrasonic Ranger, and returns the result.
+//
+int Read_Ranger(void) {
+  unsigned char info[2] = {'\0'}; // Space for us to read information from ranger
+  int range = 0; // Inititalize the range value to 0.
+  unsigned char addr = 0xE0; // Address of the ranger
+
+  i2c_read_data(addr, 2, info, 2); // Read 2 bytes (size of an unsigned int) starting at register 2
+  range = (((int)info[0] << 8) | info[1]); // Convert the two bytes of data to one short int
+  return range;
+}
+
+//-----------------------------------------------------------------------------
+// Drive_Motor
+//-----------------------------------------------------------------------------
+//
+// Vary the pulsewidth based on the user input to change the speed
+// of the drive motor.
+//
+void Drive_Motor(int range) {
+  if (range >= 40 && range <= 50) { // Range is between 40 and 50 cm
+    MOTOR_PW = DRIVE_PW_NEUT;
+  } else if (range < 40) { // Set forward speed
+    if (range < 10) { // Don't allow range to be less than 10 cm
+      range = 10;
+    }
+    MOTOR_PW = (((40 - range) * 246) / 10) + DRIVE_PW_NEUT; // Varies linearly based on range between DRIVE_PW_MAX and DRIVE_PW_NEUT
+  } else { // Set reverse speed
+    if (range > 90) { // Don't allow range to be greater than 90 cm
+      range = 90;
+    }
+
+    MOTOR_PW = (((50 - range) * 184) / 10) + DRIVE_PW_NEUT; // Varies linearly based on range between DRIVE_PW_MIN and DRIVE_PW_NEUT
+  }
+  PCA0CPL2 = 0xFFFF - MOTOR_PW;
+  PCA0CPH2 = (0xFFFF - MOTOR_PW) >> 8;
+}
+
+//-----------------------------------------------------------------------------
+// ReadCompass
+//-----------------------------------------------------------------------------
+//
+// Fuction to read the electronic compass.
+//
+unsigned int ReadCompass() {
+	unsigned char addr = 0xC0; // address of the sensor
+	unsigned char Data[2]; // array with length of 2
+	unsigned int heading; // the heading returned in degrees between 0 and 3599
+	i2c_read_data(addr, 2, Data, 2); // reads 2 bytes into Data[]
+	heading = (((unsigned int)Data[0] << 8) | Data[1]); //combines the two numbers into degrees accurate to 1/10 of a degree
+	printf("Current heading is %d\r\n", heading); //prints current heading
+	return heading; //return heading (in degrees)
+}
+
+//-----------------------------------------------------------------------------
+// Steer
+//-----------------------------------------------------------------------------
+//
+// Fuction to turn the wheels towards desired heading.
+//
+void Steer(unsigned int current_heading) {
+	unsigned int k = 2;
+	signed int error = desired_heading - current_heading;
+	if (error < -1800) { // If error is too low (car spun around past 1 cycle), add 360 degrees
+		error += 3600;
+	}	else if (error > 1800) { // If error is too high, add 360 degrees
+		error -= 3600;
+	}
+
+	if (error <= 1800 && error > 0) { // If error is above 0, set PW based off of k factor
+		STEER_PW = STEER_PW_NEUT - k*(error)/3;
+	}	else if (error <= 0 && error >= -1800) { // If error is below 0, set PW based off of k factor
+		STEER_PW = k*(-error)/3 + STEER_PW_NEUT;
+	}
+  
+	printf("Steering Pulsewidth (PW) is %u\r\n\n", STEER_PW); //Prints PW
+	PCA0CPL0 = 0xFFFF - STEER_PW;
+	PCA0CPH0 = (0xFFFF - STEER_PW) >> 8;
+}
+
+//-----------------------------------------------------------------------------
+// PCA_ISR
+//-----------------------------------------------------------------------------
+//
+// Interrupt Service Routine for Programmable Counter Array Overflow Interrupt
+//
+void PCA_ISR ( void ) interrupt 9 {
+  if (CF) {
+    // Reset PCA to the correct start value (65,535 - PCA_COUNTS)
+    PCA0L = 0xFFFF - PCA_COUNTS;
+    PCA0H = (0xFFFF - PCA_COUNTS) >> 8;
+    // Increment D_Counts variable (used for waiting 80 ms)
+    D_Counts++;
+    // Increment S_Counts variable (used for waiting 40 ms)
+    S_Counts++;
+    // Increment Overflows variable (used for waiting 1 s)
+    Overflows++;
+    if (D_Counts >= 4) {
+      new_range = 1; // signal start of read operation
+      D_Counts = 0;  //Reset Counts
+  	}
+    if (S_Counts >= 2) {
+      new_heading = 1;
+      S_Counts = 0;
+    }
+    CF = 0;
+  } else {
+    PCA0CN &= 0xC0; // all other type 9 interrupts
+  }
+}
